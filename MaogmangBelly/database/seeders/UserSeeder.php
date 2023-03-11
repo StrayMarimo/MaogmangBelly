6561cb729c403773f4141cc1dec5de7932a2e521
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-03-11 16:24:16 +0800

    chore: config mysql db

---
+++ MaogmangBelly/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Dummy Data
+        DB::table('users')->insert([
+            'name' => 'roronoa zoro',
+            'email' => 'marimo@email.com',
+            'password' => Hash::make('12345')
+        ]);
+    }
+}
