d326e40f5b410d14acab3d72b6109c0477ac035d
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-03-11 14:55:09 +0800

    feat: added initial structure

--- MaogmangBelly/resources/views/login.blade.php
+++ MaogmangBelly/resources/views/login.blade.php
@@ -1,4 +1,20 @@
 @extends('master')
 @section("content")
-<h1> Login Page </h1>
+<div class="container custom-login">
+    <div class="row d-flex justify-content-center">
+        <div class="col-sm-4">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-default border border-1">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
