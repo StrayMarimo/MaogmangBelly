6d7911fca7fe151faf56a909a3e1f0c9e02b03b5
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-04-13 00:13:51 +0800

    about redesign

--- MaogmangBelly/resources/views/layouts/about/about.blade.php
+++ MaogmangBelly/resources/views/layouts/about/about.blade.php
@@ -2,25 +2,28 @@
 @section('content')
 <div class="about">
     <div class="container-fluid text-left p-5">
-        <div class="row">
-            <div class="col">
-                <p class="text-uppercase">We are MAOGMANG BELLY</p>
-
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci nibh, maximus a interdum eget, tempor a est. Etiam eleifend, nibh a commodo porttitor, 
-                    dolor enim dapibus orci, sit amet posuere dui magna ac mi. Fusce dapibus commodo dignissim. Nullam non libero orci. Proin suscipit, risus vitae sollicitudin 
-                    imperdiet, turpis ex dapibus sapien, id blandit velit nisl quis odio. Nullam vitae bibendum urna, nec pulvinar odio. Cras aliquet turpis sed erat luctus, a 
-                    suscipit est laoreet. Sed vel consectetur leo. Aenean rutrum, arcu condimentum eleifend volutpat, ligula orci placerat arcu, finibus cursus turpis sem molestie 
-                    mauris. Morbi et libero turpis. Sed varius sapien augue. Nullam libero quam, bibendum ut finibus ut, tempor at arcu. Mauris iaculis, purus vel sollicitudin 
-                    congue, est urna hendrerit nibh, vel ullamcorper velit nulla eget enim. Nullam posuere libero maximus dolor ultrices consectetur. Pellentesque tincidunt nisi 
-                    sit amet lectus pulvinar viverra. Nullam ultrices sem ac mattis pharetra.
-                </p>
-            </div>
-            <div class="col">
-                <img src="/assets/Beef-Tapa.jpg" class="img-fluid" alt="">
+        <div class="about-box">
+            <div class="row">
+                <div class="col">
+                    <p class="we-are">We are</p>
+                    <p class="brandname">MAOGMANG BELLY</p>
+                    <p class="story">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci nibh, maximus a interdum eget, tempor a est. Etiam eleifend, nibh a commodo porttitor, 
+                        dolor enim dapibus orci, sit amet posuere dui magna ac mi. Fusce dapibus commodo dignissim. Nullam non libero orci. Proin suscipit, risus vitae sollicitudin 
+                        imperdiet, turpis ex dapibus sapien, id blandit velit nisl quis odio. Nullam vitae bibendum urna, nec pulvinar odio. Cras aliquet turpis sed erat luctus, a 
+                        suscipit est laoreet. Sed vel consectetur leo. Aenean rutrum, arcu condimentum eleifend volutpat, ligula orci placerat arcu, finibus cursus turpis sem molestie 
+                        mauris. Morbi et libero turpis. Sed varius sapien augue. Nullam libero quam, bibendum ut finibus ut, tempor at arcu. Mauris iaculis, purus vel sollicitudin 
+                        congue, est urna hendrerit nibh, vel ullamcorper velit nulla eget enim. Nullam posuere libero maximus dolor ultrices consectetur. Pellentesque tincidunt nisi 
+                        sit amet lectus pulvinar viverra. Nullam ultrices sem ac mattis pharetra.
+                    </p>
+                </div>
+                <div class="col">
+                    <div class="col2">
+                        <img src="/assets/logo.png" class="img-fluid" alt="">
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 </div>
 @endsection
