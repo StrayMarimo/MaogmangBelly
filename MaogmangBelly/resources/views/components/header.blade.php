673dabb4169a3465c6d42629cf5a655755bd1802
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-04-12 22:33:03 +0800

    home home

--- MaogmangBelly/resources/views/components/header.blade.php
+++ MaogmangBelly/resources/views/components/header.blade.php
@@ -1,4 +1,3 @@
-<div class="vignette"></div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-black">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
