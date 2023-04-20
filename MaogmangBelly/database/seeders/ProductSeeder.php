cab12be346f026942fda3e90c1527c0754bec4c1
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-04-20 15:34:14 +0800

    feat: added price for 10 people in products table

--- MaogmangBelly/database/seeders/ProductSeeder.php
+++ MaogmangBelly/database/seeders/ProductSeeder.php
@@ -13,11 +13,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        // Dummy Data
-        DB::table('products')->insert([[
+
+        DB::table('products')->insert([
+            [
                 'name' => 'Baked Macaroni',
                 'description' => "A pasta dish made with elbow macaroni, cheese, and breadcrumbs, baked until golden brown and bubbly.",
-                'price' => 900,
+                'price' => 90,
+                'price_10pax' => 900,
                 'price_20pax' => 1800,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -29,7 +31,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Classic Lasagna',
                 'description' => "A layered Italian pasta dish made with lasagna noodles, meat or vegetable sauce, ricotta cheese, and mozzarella cheese, baked until hot and gooey.",
-                'price' => 950,
+                'price' => 95,
+                'price_10pax' => 950,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -41,7 +44,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Baked Spaghetti',
                 'description' => "A baked pasta dish similar to baked macaroni, but made with spaghetti noodles and a tomato-based sauce with ground meat or sausage.",
-                'price' => 900,
+                'price' => 90,
+                'price_10pax' => 900,
                 'price_20pax' => 1800,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -53,7 +57,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Classic Pinoy Spaghetti',
                 'description' => "A popular Filipino version of spaghetti, sweet and savory tomato-based sauce with ground meat, hotdogs, and sometimes liver spread, topped with grated cheese.",
-                'price' => 850,
+                'price' => 85,
+                'price_10pax' => 850,
                 'price_20pax' => 1700,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -65,7 +70,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Chicken Alfredo',
                 'description' => "An Italian-American pasta dish made with fettuccine pasta, chicken, and a creamy sauce made with butter, cream, and parmesan cheese, often seasoned with garlic and parsley.",
-                'price' => 950,
+                'price' => 95,
+                'price_10pax' => 950,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -77,7 +83,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Spicy Shrimp Pesto',
                 'description' => "A pasta dish made with linguine noodles, pesto sauce, and sautéed shrimp, spiced up with red pepper flakes.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -89,7 +96,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Pancit Malabon',
                 'description' => "A Filipino noodle dish made with thick rice noodles, seafood, and a savory sauce made with shrimp paste, fish sauce, and annatto oil.",
-                'price' => 900,
+                'price' => 90,
+                'price_10pax' => 900,
                 'price_20pax' => 1800,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -101,7 +109,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Classic Meatballs Spaghetti',
                 'description' => "A classic Italian-American dish made with spaghetti noodles, meatballs made with a mixture of ground beef and pork, and a tomato-based sauce seasoned with garlic, onion, and herbs.",
-                'price' => 900,
+                'price' => 90,
+                'price_10pax' => 900,
                 'price_20pax' => 1800,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -113,7 +122,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Oriental Stir-Fried Noodles',
                 'description' => "A dish made with stir-fried egg noodles, vegetables, and protein (e.g., chicken, beef, shrimp) cooked with a variety of Asian seasonings such as soy sauce, oyster sauce, and sesame oil.",
-                'price' => 850,
+                'price' => 85,
+                'price_10pax' => 850,
                 'price_20pax' => 1700,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -126,7 +136,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Chicken Cordon Bleu w/ Garlic Cheese Sauce',
                 'description' => "A dish made with chicken breast stuffed with ham and cheese, breaded and baked until golden, served with a garlicky cheese sauce.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1950,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -138,7 +149,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Crispy Korean Honey Soy Chicken',
                 'description' => "A Korean-inspired fried chicken dish coated in a sweet and savory honey soy sauce, with a crispy outer layer.",
-                'price' => 900,
+                'price' => 90,
+                'price_10pax' => 900,
                 'price_20pax' => 1800,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -150,7 +162,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Chicken Fingers with 2 Special Sauce',
                 'description' => "Deep-fried chicken tenders served with two dipping sauces, usually honey mustard and barbecue sauce.",
-                'price' => 900,
+                'price' => 90,
+                'price_10pax' => 900,
                 'price_20pax' => 1800,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -162,7 +175,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Buttered Chicken in Parmesan Corn & Potatoes',
                 'description' => "A dish made with pan-seared chicken in a buttery parmesan sauce served with roasted corn and potatoes.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -174,7 +188,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Fish Stick in Sweet and Sour Sauce',
                 'description' => "A dish made with breaded and fried fish sticks served with a tangy sweet and sour sauce.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -186,7 +201,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Oriental Chicken Mushroom Stir-fry',
                 'description' => "A dish made with stir-fried chicken and mushrooms in a savory oriental sauce, served over rice or noodles.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -198,7 +214,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Fish Lumpia',
                 'description' => "A Filipino version of spring rolls made with ground fish, vegetables, and spices wrapped in a thin wrapper and deep-fried until golden brown.",
-                'price' => 1500,
+                'price' => 150,
+                'price_10pax' => 1500,
                 'price_20pax' => 2900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -210,7 +227,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Seafood Paella',
                 'description' => "A Spanish rice dish made with saffron-seasoned rice, seafood (e.g., shrimp, mussels, squid), and vegetables, cooked in a paella pan.",
-                'price' => 1500,
+                'price' => 150,
+                'price_10pax' => 1500,
                 'price_20pax' => 2900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -222,7 +240,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Cajun Seafood',
                 'description' => "A spicy seafood dish seasoned with Cajun spices, usually made with shrimp, crawfish, and/or crab.",
-                'price' => 1500,
+                'price' => 150,
+                'price_10pax' => 1500,
                 'price_20pax' => 2900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -235,7 +254,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Crispy Pork Kare-Kare',
                 'description' => "A Filipino dish made with crispy pork belly served with a peanut sauce made with vegetables and bagoong (shrimp paste).",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1950,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -247,7 +267,9 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Pork Cordon Bleu',
                 'description' => "A dish made with pork loin stuffed with ham and cheese, breaded and fried until crispy.",
-                'price' => 850,
+                'price' => 85,
+                'price_10pax' => 850,
+                'price_10pax' => 900,
                 'price_20pax' => 1600,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -259,7 +281,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Beef with Broccoli',
                 'description' => "A Chinese stir-fry dish made with sliced beef and broccoli cooked in a savory brown sauce made with soy sauce and oyster sauce.",
-                'price' => 1200,
+                'price' => 120,
+                'price_10pax' => 1200,
                 'price_20pax' => 2300,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -271,7 +294,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Beef Kare-Kare',
                 'description' => "A Filipino stew made with tender beef, vegetables, and a thick peanut sauce, usually served with bagoong (shrimp paste).",
-                'price' => 1200,
+                'price' => 120,
+                'price_10pax' => 1200,
                 'price_20pax' => 2300,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -283,7 +307,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Lumpiang Shanghai',
                 'description' => " A Filipino version of spring rolls made with ground pork or beef, vegetables, and spices wrapped in a thin wrapper and deep-fried until crispy.",
-                'price' => 600,
+                'price' => 60,
+                'price_10pax' => 600,
                 'price_20pax' => 1100,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -295,7 +320,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Crispy Sweet and Sour Pork',
                 'description' => "A Chinese dish made with crispy fried pork pieces coated in a sweet and sour sauce made with pineapple, bell peppers, and vinegar.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -308,7 +334,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Nacho Bake',
                 'description' => "A layered baked dish made with tortilla chips, ground beef, cheese, and other toppings, typically served as an appetizer or snack.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1900,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -320,7 +347,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Oriental Stir-fried Mixed Veggies with Quail',
                 'description' => "A dish made with stir-fried mixed vegetables and quail meat, cooked with a variety of Asian seasonings such as soy sauce and sesame oil.",
-                'price' => 500,
+                'price' => 50,
+                'price_10pax' => 500,
                 'price_20pax' => 1000,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -332,7 +360,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Thai Sticky Rice with Mango',
                 'description' => "A Thai dessert made with glutinous rice cooked in coconut milk and served with sliced mango, often topped with a sweet syrup made with palm sugar.",
-                'price' => 600,
+                'price' => 60,
+                'price_10pax' => 600,
                 'price_20pax' => 1100,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -344,7 +373,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Coffee Jelly',
                 'description' => "A Japanese dessert made with coffee-flavored gelatin cubes served with whipped cream and milk.",
-                'price' => 700,
+                'price' => 70,
+                'price_10pax' => 700,
                 'price_20pax' => 1400,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -356,7 +386,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Mango Tapioca',
                 'description' => "A Filipino dessert made with tapioca pearls cooked in coconut milk and served with fresh mangoes and a sweet syrup.",
-                'price' => 700,
+                'price' => 70,
+                'price_10pax' => 700,
                 'price_20pax' => 1400,
                 'stock' => 20,
                 'total_sold' => 0,
@@ -368,7 +399,8 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Potato Salad',
                 'description' => " A cold salad made with boiled potatoes, mayonnaise, and other ingredients such as celery, onions, and pickles, often served as a side dish.",
-                'price' => 1000,
+                'price' => 100,
+                'price_10pax' => 1000,
                 'price_20pax' => 1950,
                 'stock' => 20,
                 'total_sold' => 0,
