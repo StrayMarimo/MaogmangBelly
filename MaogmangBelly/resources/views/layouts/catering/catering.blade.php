cd76051dc952465a1937b1ade4c5dd1c1060af5e
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-04-20 21:35:10 +0800

    catering redesign

--- MaogmangBelly/resources/views/layouts/catering/catering.blade.php
+++ MaogmangBelly/resources/views/layouts/catering/catering.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.app')
 @section('content')
-catering page
+<div style="height: 120vh" class="text-light-2">
+    <div class="border1">
+        <h1 class="tagline">Fresh Flavors, <br>
+            Creative Catering</h1> <br>
+        <h3 class="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+        </h3>
+    </div>
+</div>
+<div style="height: 120vh" class="green-sec">
+    <h1 class="tag2">WHAT WE DO</h1>
+    <p class="cap1 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br>
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+
+    <div class="card-deck">
+        <div class="box2 card">
+            <div class="card-body">
+            <h2 class="capcap card-title mb-5">Drop Off<br>Catering</h5>
+            <p class="card-text">Sed porttitor lectus nibh. Curabitur non 
+                nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in faucibus orci</p>
+            </div>
+        </div>
+        <div class="box2 card">
+            <div class="card-body">
+            <h2 class="capcap card-title mb-5">Event<br>Staffing</h5>
+            <p class="card-text">Sed porttitor lectus nibh. Curabitur non 
+                nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in faucibus orci</p>
+            </div>
+        </div>
+        <div class="box2 card">
+            <div class="card-body">
+            <h2 class="capcap card-title mb-5">Corporate<br>Events</h5>
+            <p class="card-text">Sed porttitor lectus nibh. Curabitur non 
+                nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in faucibus orci</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
