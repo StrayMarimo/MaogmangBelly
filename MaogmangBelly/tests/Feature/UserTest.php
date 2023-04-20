92ede72740fb7610e206e8fbbf90ff812ad017bb
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-04-20 23:55:51 +0800

    tests: user registration and login

--- MaogmangBelly/tests/Feature/UserTest.php
+++ MaogmangBelly/tests/Feature/UserTest.php
@@ -3,18 +3,52 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_register()
+    {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $validator = Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $this->assertTrue($validator->passes());
+
+        $this->post('/register', $data)
+            ->assertStatus(302)
+            ->assertRedirect('/');
+    }
+
+    public function test_user_can_login()
+    {
+        $user = User::factory()->create();
+
+        // Log in the user
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+
+        // Assert that the response is successful
+        $response->assertStatus(302);
+
+        // Assert that the user was authenticated
+        $this->assertAuthenticatedAs($user);
     }
 }
