db480f6a997f44b7ab35be214a0304204dc2f24f
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-04-20 17:10:42 +0800

    added navtabs test cases

---
+++ MaogmangBelly/tests/Feature/NavBarPagesTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Route;
+use Tests\TestCase;
+use App\Models\User;
+
+
+
+class NavBarPagesTest extends TestCase
+{
+
+    use DatabaseTransactions;
+   
+    public function test_home_tab(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_catering_tab(): void
+    {
+        $response = $this->get('/catering');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_reservations_tab(): void
+    {
+        $response = $this->get('/reservations');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_about_tab(): void
+    {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_order_tab_if_has_user(): void
+    {   
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/order');
+
+        $response->assertStatus(200);
+      
+    }
+
+    public function test_order_tab_if_no_user(): void
+    {
+        $response = $this->get('/order');
+
+        $response->assertStatus(302); // checks if redirect occured
+        $response->assertRedirect('/login');
+
+    }
+
+    public function test_contact_tab(): void
+    {
+        $response = $this->get('/contact');
+        $response->assertStatus(200);
+    } 
+
+    public function test_login_tab_if_no_user(): void
+    {
+
+        $this->assertTrue(Route::has('login'));
+    }
+
+    public function test_login_tab_if_has_user(): void
+    {
+        $user = User::factory()->create();
+      
+        $response = $this->actingAs($user)
+            ->get('/login');
+
+        $response->assertRedirect('/');
+    }
+
+
+}
