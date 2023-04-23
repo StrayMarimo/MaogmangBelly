80de090ef90c29b7e3c30e2758424dde319ac1d7
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-04-23 16:24:52 +0800

    style: temp style for product cards

--- MaogmangBelly/resources/views/layouts/products/product_card.blade.php
+++ MaogmangBelly/resources/views/layouts/products/product_card.blade.php
@@ -1,5 +1,5 @@
 <div class="card mx-4">
-    <img src="{{ url('/assets/product_assets/'.$product['gallery']) }}" class="card-img-top" style="width: 300px; height: 200px;">
+    <img src="{{ url('/assets/product_assets/'.$product['gallery']) }}" class="card-img-top" style="height:30vh;">
     <div class="card-body">
         <h5 class="card-title">{{$product['name']}}</h5>
         <p class="card-text">{{$product['description']}}</p>
