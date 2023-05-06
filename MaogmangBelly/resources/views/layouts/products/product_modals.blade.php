ccae7ea40074bd34f1a20cbce92538f1857159f7
maldkun <matthewconsulta842@gmail.com>
2023-05-06 23:28:25 +0800

    Edit modal fixed

--- MaogmangBelly/resources/views/layouts/products/product_modals.blade.php
+++ MaogmangBelly/resources/views/layouts/products/product_modals.blade.php
@@ -111,7 +111,7 @@
 
 <!-- Update Product Modal -->
 <div class="modal fade" id="editProductModal" data-categories="" tabindex="-1" aria-labelledby="edit-product-modal-label"
-    aria-hidden="true">
+    aria-hidden="true" style="font-family: 'Franklin Gothic Medium';">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <!-- Modal header -->
@@ -120,53 +120,81 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <!-- Edit Product form -->
-            <form action="{{ route('edit_product')}}" method="POST" enctype="multipart/form-data" id="editProductForm">
+            <form action="{{ route('edit_product')}}" method="POST" enctype="multipart/form-data" id="editProductForm" style="margin: 20px; line-height: 2.5;">
                 @csrf
                 <!-- Save product ID -->
                 <input type="hidden" id="productId" value="" name="product_id" required>
                 <!-- Save category ID -->
                 <input type="hidden" id="productCategoryId" value="" name="category_id" required>
-                <div class="form-group">
+                <div class="form-group mb-2">
                     <select class="form-control" id="selectCategoryUpdate" required>
                         <!-- dynamically populate options here -->
                     </select>
                 </div>
                 <!-- Input for product name -->
-                Name: <input type="text" id="name" name="product_name" required> <br />
+                <div class="form-box form-floating mb-3 col-md-auto">
+                    <input type="text" id="name" name="product_name"
+                        class="boxbox form-control @error('product_name') is-invalid @enderror"
+                        value="{{ old('product_name') }}" required>
+                    <label for="floatingInput">Name</label>
+                </div>
                 <!-- Input for product description -->
-                Description: <textarea rows="4" cols="50" id="description" name="product_desc"
-                    required></textarea> <br />
+                <div class="form-box form-floating mb-3 col-md-auto">
+                    <textarea rows="4" cols="50" id="description" name="product_desc" class="form-control @error('product_desc') is-invalid @enderror"
+                        required>{{ old('product_desc') }}</textarea>
+                    <label for="floatingTextarea">Description</label>
+                </div>
                 <!-- Input for product price -->
-                Price: <input type="number" id="price" name="product_price" min="0" max="1000000" step="0.01"
-                    required> <br />
+                <div class="form-box form-floating mb-3 col-md-auto">
+                    <input type="number" id="price" name="product_price" min="0" max="1000000" step="0.01"
+                        class="boxbox form-control @error('product_price') is-invalid @enderror"
+                        value="{{ old('product_price') }}" required>
+                    <label for="floatingInput">Price</label>
+                </div>
                 <!-- Input for product price for 10pax-->
-                Price(10pax): <input type="number" id="price10" name="product_price_10" min="0" max="1000000"
-                    step="0.01" required> <br />
+                <div class="form-box form-floating mb-3 col-md-auto">
+                    <input type="number" id="price10" name="product_price_10" min="0" max="1000000"
+                        step="0.01" class="boxbox form-control @error('product_price_10') is-invalid @enderror"
+                        value="{{ old('product_price_10') }}" required>
+                    <label for="floatingInput">Price (10pax)</label>
+                </div>
                 <!-- Input for product price for 20pax-->
-                Price(20pax): <input type="number" id="price20" placeholder="120.50" name="product_price_20" min="0" max="1000000"
-                    step="0.01" required> <br />
-                <!-- Input for product stock -->
-                Stock: <input type="number" id="stock" name="product_stock" required> <br />
+                <div class="form-box form-floating mb-3 col-md-auto">
+                    <input type="number" id="price20" placeholder="120.50" name="product_price_20" min="0" max="1000000"
+                        step="0.01" class="boxbox form-control @error('product_price_20') is-invalid @enderror"
+                        value="{{ old('product_price_20')}}" required>
+                        <label for="floatingInput">Price (20pax)</label>
+                </div>
+                <div class="form-box form-floating mb-3 col-md-auto">
+                    <input id="stock" type="number" class="boxbox form-control @error('product_stock') is-invalid @enderror" name="product_stock" min="0" max="1000000" value="{{ old('product_stock') }}" required autocomplete="product_stock" autofocus>
+                    <label for="stock">Stock</label>
+                    @error('product_stock')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
                 <!-- Input for product gallery -->
-                <input type="checkbox" name="is_trending" id="isTrending"> Trending Product <br />
-                <input type="checkbox" name="is_featured" id="isFeatured"> Featured Product <br />
+                <input type="checkbox" name="is_trending" id="isTrending" class="form-check-input">
+                <label for="isTrending" class="form-check-label">Trending Product</label> <br />
+                <input type="checkbox" name="is_featured" id="isFeatured" class="form-check-input">
+                <label for="isFeatured" class="form-check-label">Featured Product</label> <br />
                 <label for="img">Upload Product Image</label>
-                <div class="row">
-                    <div class="col-sm-6 ">
-                        <img id="preview-image-before-upload" src="{{Vite::image('logo.png')}}
-                    " alt="preview image" style="height: 200px; width: 200px; object-fit: cover;" class=" mb-2">
-                        <input class=" form-control @error('img') is-invalid @enderror " type="file" name="img"
-                            value="{{Vite::image('logo.png')}}" id="img">
+
+                <div class="row mb-3">
+                    <div class="col-sm-6">
+                        <img id="preview-image-before-upload" src="{{ asset('/assets/product_assets/spaghetti.jpg') }}" alt="preview image" style="height: 216px; width: 288px; object-fit: cover;" class="mb-2">
+                        <input class="form-control @error('img') is-invalid @enderror" type="file" name="img" id="img" accept="image/*" required>
                         <!-- Display error message if file upload fails -->
                         @error('img')
-                        <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert" style="mb-2">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
                     </div>
-                </div>
-                <!-- Modal footer -->
-                <div class="modal-footer">
+                    </div>
+                    <!-- Modal footer -->
+                <div class="modal-footer mt-2">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save Changes</button>
                 </div>
