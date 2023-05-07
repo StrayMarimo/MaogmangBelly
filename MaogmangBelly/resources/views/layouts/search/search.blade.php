1d1ab289047c231d3718557ead9276bad7e46377
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-05-07 22:21:28 +0800

    style: search style minor fix

--- MaogmangBelly/resources/views/layouts/search/search.blade.php
+++ MaogmangBelly/resources/views/layouts/search/search.blade.php
@@ -10,13 +10,13 @@
           @foreach($chunk as $item)
             <div class="col-sm-4">
               <div class="card mx-4">
-                <a href="{{ route('product_details', ['id' => $item['id']]) }}">
+                <a href="{{ route('product_details', ['id' => $item['id']]) }}" id="searchCard">
                   <img class="card-img-top" src="{{ asset('assets/product_assets/'.$item['gallery']) }}" alt="{{ $item['name'] }}" style="object-fit: cover; width: 100%; height: 30vh;">
-                </a>
                 <div class="card-body">
                   <h5 class="card-title">{{$item['name']}}</h5>
                   <p class="card-text" style="max-height: 6em; overflow: hidden; text-overflow: ellipsis;">{{$item['description']}}</p>
                 </div>
+                </a>
               </div>
             </div>
           @endforeach
@@ -27,48 +27,3 @@
 </div>
 
 @endsection
-
-
-<!-- <div class="custom-product">
-     <div class="col-sm-4 mx-2">
-         <a href="/">Go Back</a>
-     </div>
-     <div class="col-sm-4 mx-2">
-        <div class="trending-wrapper">
-            <h4>Result for Products</h4>
-            @foreach($products as $item)
-            <div class="card">
-              <a href="{{ route('product_details', ['id' => $item['id']]) }}">
-                <img class="card-img-top" src="{{ asset('assets/product_assets/'.$item['gallery']) }}" alt="{{ $item['name'] }}">
-              </a>
-              <div class="card-body">
-                <h5 class="card-title">{{$item['name']}}</h5>
-                <p class="card-text">{{$item['description']}}</p>
-              </div>
-            </div>
-            @endforeach
-          </div>
-     </div>
-</div> -->
-
-<!-- <div class="custom-product">
-     <div class="col-sm-4 mx-2">
-         <a href="/">Go Back</a>
-     </div>
-     <div class="col-sm-4 mx-2">
-        <div class="trending-wrapper">
-            <h4>Result for Products</h4>
-            @foreach($products as $item)
-            <div class="searched-item">
-              <a href="{{ route('product_details', ['id' => $item['id']]) }}">
-              <img class="trending-image" src="{{ asset('assets/product_assets/'.$item['gallery']) }}">
-              <div class="">
-                <h2>{{$item['name']}}</h2>
-                <h5>{{$item['description']}}</h5>
-              </div>
-            </a>
-            </div>
-            @endforeach
-          </div>
-     </div>
-</div> -->
