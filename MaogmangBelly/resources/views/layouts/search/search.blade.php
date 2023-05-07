075a4c58cbc74e028662ffcffc5e38d9f55a85e5
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-05-07 15:30:34 +0800

    search redesign

--- MaogmangBelly/resources/views/layouts/search/search.blade.php
+++ MaogmangBelly/resources/views/layouts/search/search.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.app')
 @section("content")
 <div class="custom-product">
-  <div class="col-sm-4 mx-2">
-    <a href="/">Go Back</a>
-    <h4>Result for Products</h4>
-  </div>
+  <a href="/" style="font-family: lexend; margin-left: 30px; margin-bottom: 50px">Go Back</a>
+  <h4 class="mb-3 mt-2 tag2" style="font-family: lexend">Result for Products</h4>
   <div class="col-sm-12 mx-2">
-    <div style="background-image: url({{Vite::image('search_bg.jpg')}})" class="trending-wrapper">
+    <div class="trending-wrapper">
       @foreach($products->chunk(3) as $chunk)
         <div class="row">
           @foreach($chunk as $item)
             <div class="col-sm-4">
               <div class="card mx-4">
                 <a href="{{ route('product_details', ['id' => $item['id']]) }}">
-                  <img class="card-img-top" src="{{ asset('assets/product_assets/'.$item['gallery']) }}" alt="{{ $item['name'] }}" style="height:30vh;">
+                  <img class="card-img-top" src="{{ asset('assets/product_assets/'.$item['gallery']) }}" alt="{{ $item['name'] }}" style="object-fit: cover; width: 100%; height: 30vh;">
                 </a>
                 <div class="card-body">
                   <h5 class="card-title">{{$item['name']}}</h5>
