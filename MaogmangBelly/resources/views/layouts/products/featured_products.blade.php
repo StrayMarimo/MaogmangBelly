bafa321dc744339e86393cc355b91491b48e990c
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-05-08 23:41:25 +0800

    product overhaul

--- MaogmangBelly/resources/views/layouts/products/featured_products.blade.php
+++ MaogmangBelly/resources/views/layouts/products/featured_products.blade.php
@@ -1,4 +1,4 @@
-<div class="custom-product col-6">
+<!-- <div class="custom-product col-6">
     <div id="product-showcase" class="carousel slide">
         <div class="carousel-inner">
             @foreach ($featured_products as $item)
@@ -40,5 +40,48 @@
             });
         </script>
     </div>
-</div>
+</div> -->
 
+<div id="carouselExampleCaptions" class="carousel h-150 slide" data-bs-ride="false">
+    <div id="product-showcase" class="carousel-inner">
+        <div class="carousel-inner">
+            @foreach ($featured_products as $item)
+            <div class="carousel-item {{$loop->index == 1 ? 'active' : ''}}">
+                <a href="{{ route('product_details', ['id' => $item['id']]) }}">
+                    <img class="d-block w-100 brightness" src="{{ url('/assets/product_assets/'.$item['gallery']) }}"
+                        class="d-block w-100">
+                    <div class="carousel-caption d-none d-md-block slider-text">
+                        <h1 style="font-family: 'Lexend';">{{$item['name']}}</h1>
+                        <p class="cap1 mb-2">{{$item['description']}}</p>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+        <button class="carousel-control-prev mx-5" type="button" data-bs-target="#product-showcase"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button id="nextBtn" class="carousel-control-next mx-5" type="button" data-bs-target="#product-showcase"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        <script>
+            $(document).ready(function() {
+                var intervalId = setInterval(function() {
+                    $('#nextBtn').click();
+                }, 4000);
+
+                // When the user clicks on the previous button, reset the timer
+                $('.carousel-control-prev').click(function() {
+                    clearInterval(intervalId);
+                    intervalId = setInterval(function() {
+                        $('#nextBtn').click();
+                    }, 4000);
+                });
+            });
+        </script>        
+    </div>
+</div>
