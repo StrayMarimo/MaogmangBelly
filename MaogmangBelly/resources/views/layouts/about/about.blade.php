2434ff414488ea12c049ee162f043522dd26d1f7
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-05-09 14:32:08 +0800

    about overhaul

--- MaogmangBelly/resources/views/layouts/about/about.blade.php
+++ MaogmangBelly/resources/views/layouts/about/about.blade.php
@@ -5,16 +5,21 @@
         <div class="about-box mx-auto">
             <div class="row mx-5">
                 <div class="col">
-                    <p class="we-are">We are</p>
+                    <p class="we-are mb-4">We are</p>
                     <p class="brandname">MAOGMANG BELLY</p>
                     <p class="story">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci nibh, maximus a interdum eget, tempor a est. Etiam eleifend, nibh a commodo porttitor, 
-                        dolor enim dapibus orci, sit amet posuere dui magna ac mi. Fusce dapibus commodo dignissim. Nullam non libero orci. Proin suscipit, risus vitae sollicitudin 
-                        imperdiet, turpis ex dapibus sapien, id blandit velit nisl quis odio. Nullam vitae bibendum urna, nec pulvinar odio. Cras aliquet turpis sed erat luctus, a 
-                        suscipit est laoreet. Sed vel consectetur leo. Aenean rutrum, arcu condimentum eleifend volutpat, ligula orci placerat arcu, finibus cursus turpis sem molestie 
-                        mauris. Morbi et libero turpis. Sed varius sapien augue. Nullam libero quam, bibendum ut finibus ut, tempor at arcu. Mauris iaculis, purus vel sollicitudin 
-                        congue, est urna hendrerit nibh, vel ullamcorper velit nulla eget enim. Nullam posuere libero maximus dolor ultrices consectetur. Pellentesque tincidunt nisi 
-                        sit amet lectus pulvinar viverra. Nullam ultrices sem ac mattis pharetra.
+                     Once a small corner deli, our restaurant has now become a beloved establishment in the 
+                     heart of the community. It all started with a family's passion for food and a desire to share it with others.
+                     <br>
+                     <br>
+                     Our founder, Leslie, grew up in a family of chefs and bakers who instilled in her a love for cooking and a respect for 
+                     quality ingredients. When she moved to this town, Leslie noticed a lack of good, affordable dining options, and decided 
+                     to take matters into her own hands.
+                     <br>
+                     <br>
+                     As word spread about the delicious food and friendly service, the restaurant quickly gained a loyal following. 
+                     Leslie's family and staff members take pride in getting to know their customers and treating them like family. 
+                     They believe that a meal is not just about the food, but also about the company and the memories created around the table.
                     </p>
                 </div>
                 <div class="col">
