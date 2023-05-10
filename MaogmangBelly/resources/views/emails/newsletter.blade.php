576b7c074e86fd21b1898baab0f8e0d8e04fcf07
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-05-10 22:10:15 +0800

    style: redesign email newsletter

--- MaogmangBelly/resources/views/emails/newsletter.blade.php
+++ MaogmangBelly/resources/views/emails/newsletter.blade.php
@@ -1,22 +1,288 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
-    <title>Maogmang Belly</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
+    <meta name="x-apple-disable-message-reformatting"> <!-- Disable auto-scale in iOS 10 Mail entirely -->
+    <title>Welcome - [Plain HTML]</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,500" rel="stylesheet">
+    <style>
+        html,
+        body {
+            margin: 0 auto !important;
+            padding: 0 !important;
+            height: 100% !important;
+            width: 100% !important;
+        }
+
+        /* What it does: Stops email clients resizing small text. */
+        * {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+        }
+
+        /* What it does: Centers email on Android 4.4 */
+        div[style*="margin: 16px 0"] {
+            margin: 0 !important;
+        }
+
+        /* What it does: Stops Outlook from adding extra spacing to tables. */
+        table,
+        td {
+            mso-table-lspace: 0pt !important;
+            mso-table-rspace: 0pt !important;
+        }
+
+        table {
+            border-spacing: 0 !important;
+            border-collapse: collapse !important;
+            table-layout: fixed !important;
+            margin: 0 auto !important;
+        }
+
+        table table table {
+            table-layout: auto;
+        }
+
+        img {
+            -ms-interpolation-mode: bicubic;
+        }
+
+        *[x-apple-data-detectors],
+        /* iOS */
+        .x-gmail-data-detectors,
+        /* Gmail */
+        .x-gmail-data-detectors *,
+        .aBn {
+            border-bottom: 0 !important;
+            cursor: default !important;
+            color: inherit !important;
+            text-decoration: none !important;
+            font-size: inherit !important;
+            font-family: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+        }
+
+        .a6S {
+            display: none !important;
+            opacity: 0.01 !important;
+        }
+
+        img.g-img+div {
+            display: none !important;
+        }
+
+        /* What it does: Prevents underlining the button text in Windows 10 */
+        .button-link {
+            text-decoration: none !important;
+        }
+
+        @media only screen and (min-device-width: 375px) and (max-device-width: 413px) {
+            .email-container {
+                min-width: 375px !important;
+            }
+        }
+    </style>
+    <style>
+        .button-td,
+        .button-a {
+            transition: all 100ms ease-in;
+        }
+
+        .button-td:hover,
+        .button-a:hover {
+            background: #555555 !important;
+            border-color: #555555 !important;
+        }
+
+        /* Media Queries */
+        @media screen and (max-width: 480px) {
+            .fluid {
+                width: 100% !important;
+                max-width: 100% !important;
+                height: auto !important;
+                margin-left: auto !important;
+                margin-right: auto !important;
+            }
+
+            .stack-column,
+            .stack-column-center {
+                display: block !important;
+                width: 100% !important;
+                max-width: 100% !important;
+                direction: ltr !important;
+            }
+
+            /* And center justify these ones. */
+            .stack-column-center {
+                text-align: center !important;
+            }
+
+            .center-on-narrow {
+                text-align: center !important;
+                display: block !important;
+                margin-left: auto !important;
+                margin-right: auto !important;
+                float: none !important;
+            }
+
+            table.center-on-narrow {
+                display: inline-block !important;
+            }
+
+            .email-container p {
+                font-size: 17px !important;
+                line-height: 22px !important;
+            }
+        }
+    </style>
 </head>
 
-<body>
-    <h1>{{ $mailData['title'] }}</h1>
-    <p>{{ $mailData['body'] }}</p>
+<body width="100%" bgcolor="#F1F1F1" style="margin: 0; mso-line-height-rule: exactly;">
+    <center style="width: 100%; background: #F1F1F1; text-align: left;">
+        <div
+            style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;">
+            Glad to have you on board!</div>
+        <div style="max-width: 680px; margin: auto;" class="email-container">
+            <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%"
+                style="max-width: 680px;" class="email-container">
+                <tr>
+                    <td bgcolor="#333333">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <tr>
+                                <td style="padding: 30px 40px 30px 40px; text-align: center;"> <span
+                                        style="color:#fff; font-size: 30px">Maogmang Belly</span> </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td background="https://images.unsplash.com/photo-1578474846511-04ba529f0b88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80
+                        bgcolor=" #222222" align="center" valign="top"
+                        style="text-align: center; background-position: center center !important; background-size: cover !important;">
+                        <div>
+                            <table role="presentation" border="0" cellpadding="0" cellspacing="0" align="center"
+                                width="100%" style="max-width:500px; margin: auto;">
+                                <tr>
+                                    <td height="20" style="font-size:20px; line-height:20px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td align="center" valign="middle">
+                                        <table>
+                                            <tr>
+                                                <td valign="top" style="text-align: center; padding: 60px 0 10px 20px;">
+                                                    <h1
+                                                        style="margin: 0; font-family: 'Montserrat', sans-serif; font-size: 30px; line-height: 36px; color: #ffffff; font-weight: bold;">
+                                                        WELCOME!</h1>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td valign="top"
+                                                    style="text-align: center; padding: 10px 20px 15px 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #fff;">
+                                                    <p style="margin: 0;">You are now on the list -- and will be the
+                                                        first to know about our latest products and promos,
+                                                        exclusive offers and much more!</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td valign="top" align="center"
+                                                    style="text-align: center; padding: 15px 0px 60px 0px;">
+                                                    <center>
+                                                        <table role="presentation" align="center" cellspacing="0"
+                                                            cellpadding="0" border="0" class="center-on-narrow"
+                                                            style="text-align: center;">
+                                                            <tr>
+                                                                <td style="border-radius: 50px; background: #26a4d3; text-align: center;"
+                                                                    class="button-td"> <a href="http://127.0.0.1:8000/"
+                                                                        style="background: #c1272d;; border: 15px solid #c1272d;;; font-family: 'Montserrat', sans-serif; font-size: 14px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 50px; font-weight: bold;"
+                                                                        class="button-a"> <span style="color:#ffffff;"
+                                                                            class="button-link">&nbsp;&nbsp;&nbsp;&nbsp;Visit
+                                                                            Website&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </center>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-    <p>Thank you</p>
+                                </tr>
+                                <tr>
+                                    <td height="20" style="font-size:20px; line-height:20px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#ffffff">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <tr>
+                                <td style="padding: 40px 40px 20px 40px; text-align: left;">
+                                    <h1
+                                        style="margin: 0; font-family: 'Montserrat', sans-serif; font-size: 20px; line-height: 26px; color: #333333; font-weight: bold;">
+                                        YOU ARE NOW SUBSCRIBED TO OUR EMAIL NEWSLETTER!</h1>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td
+                                    style="padding: 0px 40px 20px 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; text-align: left; font-weight:bold;">
+                                    <p style="margin: 0;">Thank you for choosing Maogmang Belly.</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td
+                                    style="padding: 0px 40px 20px 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; text-align: left; font-weight:normal;">
+                                    <p style="margin: 0;">We take pride in providing exceptional culinary experiences
+                                        that are tailored to your unique tastes and preferences
+                                        offering a wide range of catering services that are designed to make your event
+                                        unforgettable.</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr> <!-- INTRO : END -->
+                <!-- CTA : BEGIN -->
+                <tr>
+                    <td bgcolor="#ffffff">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%"> <br>
+                            <tr>
+                                <td align="center"> <img
+                                        src="https://drive.google.com/uc?id=1pA3x4pPl6X96TjAAs9apzDNKKYNDw4zL"
+                                        width="37" height="37" style="display: block; border: 0px;" /> </td>
+                            </tr>
+                            <tr>
+                                <td align="center"
+                                    style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 5px 0 10px 0;">
+                                    <p style="font-size: 14px; font-weight: 800; line-height: 18px; color: #333333;">
+                                        Maogmang Belly<br> Elias Angeles Street, Naga City, Bicol</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td
+                                    style="padding: 0px 40px 10px 40px; font-family: sans-serif; font-size: 12px; line-height: 18px; color: #666666; text-align: center; font-weight:normal;">
+                                    <p style="margin: 0;">This email was sent to you from maogmangbelly@gmail.com</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td
+                                    style="padding: 0px 40px 40px 40px; font-family: sans-serif; font-size: 12px; line-height: 18px; color: #666666; text-align: center; font-weight:normal;">
+                                    <p style="margin: 0;">Copyright &copy; 2023 <b>MaogmangBelly.com</b>, All Rights
+                                        Reserved.</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </center>
 </body>
 
 </html>
