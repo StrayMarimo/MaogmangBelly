999877eb8d1f65c246bd7a9aa35a9e46994c7b40
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-05-10 15:52:37 +0800

    fixed: merge conflicts

--- MaogmangBelly/resources/js/app.js
+++ MaogmangBelly/resources/js/app.js
@@ -12,16 +12,6 @@ import './history.js';
 import.meta.glob(['../images/**']);
 
 $(document).ready(function () {
-<<<<<<< HEAD
-    // /*! Fades in page on load */
-    // $('body').css('display', 'none');
-    // $('body').fadeIn(500);
-    // // $('body').addClass('visible');
-    
-    
-=======
-
->>>>>>> 409f94fd3787cad569c6318f736fef71b78a647a
     $(window).scrollTop(0);
     var minDate = new Date();
     minDate.setDate(minDate.getDate() + 5); // add 5 days to today's date
